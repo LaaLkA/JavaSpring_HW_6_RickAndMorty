6898cefa8e9ab963f2aa3dd9060bd4c6e1b0de26
LaaLkA <n.afonchenkov@yandex.ru>
2024-12-09 16:48:26 +0300

    Initial commit

---
+++ src/main/java/org/laalka/javaspring_hw_6_rickandmorty/JavaSpringHw6RickAndMortyApplication.java
@@ -0,0 +1,13 @@
+package org.laalka.javaspring_hw_6_rickandmorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringHw6RickAndMortyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringHw6RickAndMortyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/laalka/javaspring_hw_6_rickandmorty/JavaSpringHw6RickAndMortyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.laalka.javaspring_hw_6_rickandmorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringHw6RickAndMortyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
