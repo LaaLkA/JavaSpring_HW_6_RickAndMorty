43bb68cb1a502e247170f25b7823c1b400d9c5ca
LaaLkA <n.afonchenkov@yandex.ru>
2024-12-10 16:52:07 +0300

    in progress

--- src/main/java/org/laalka/javaspring_hw_6_rickandmorty/Controller/Rest/RickAndMortyRestController.java
+++ src/main/java/org/laalka/javaspring_hw_6_rickandmorty/Controller/Rest/RickAndMortyRestController.java
@@ -3,16 +3,13 @@ package org.laalka.javaspring_hw_6_rickandmorty.Controller.Rest;
 import lombok.RequiredArgsConstructor;
 import org.laalka.javaspring_hw_6_rickandmorty.Service.RickAndMortyClient;
 import org.laalka.javaspring_hw_6_rickandmorty.model.Person;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/rickandmortyapi")
+@RequestMapping("/api")
 public class RickAndMortyRestController {
 
     private final RickAndMortyClient client;
@@ -20,5 +17,13 @@ public class RickAndMortyRestController {
     @GetMapping("/persons/{first_id}/{last_id}")
     public List<Person> getPersons(@PathVariable("first_id") String firstId, @PathVariable("last_id") String lastId) {
         return client.getPersonsStartEndId(firstId, lastId);
+    }
+
+    @GetMapping("/persons")
+    public List<Person> getCharacters(@RequestParam(defaultValue = "0") int page){
+        int pageSize = 10;
+        int startId = page * pageSize + 1;
+        int endId = startId + pageSize;
+        return client.getPersonsStartEndId(String.valueOf(startId), String.valueOf(endId));
     }
 }
--- src/main/java/org/laalka/javaspring_hw_6_rickandmorty/Controller/RickAndMortyController.java
+++ src/main/java/org/laalka/javaspring_hw_6_rickandmorty/Controller/RickAndMortyController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,14 +19,37 @@ public class RickAndMortyController {
 
     private final RickAndMortyClient rickAndMortyClient;
 
+    @GetMapping
+    public String start(){
+        return "index";
+    }
+
     @GetMapping("/persons/{first_id}/{last_id}")
     public String getCharactersByFirstLastId(@PathVariable("first_id") String firstId,
                                              @PathVariable("last_id") String lastId,
                                              Model model) {
         List<Person> persons = rickAndMortyClient.getPersonsStartEndId(firstId, lastId);
         model.addAttribute("persons", persons);
+        return "charactersByPage";
+
+
+    }
+
+    @GetMapping("/charactersByPage")
+    public String getCharactersPage(@RequestParam(defaultValue = "0") int page,
+                                    Model model) {
+        int pageSize = 10;
+        int startId = page * pageSize + 1;
+        int endId = startId + pageSize;
+
+        List<Person> persons = rickAndMortyClient.getPersonsStartEndId(String.valueOf(startId), String.valueOf(endId));
+        model.addAttribute("persons", persons);
+        model.addAttribute("page", page);
+        return "charactersByPage";
+    }
+
+    @GetMapping("persons")
+    public String getPersons(Model model) {
         return "persons";
-
-
     }
 }
